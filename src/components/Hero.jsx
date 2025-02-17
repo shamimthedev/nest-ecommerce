dcff6e301ac6261ac1a905c1bbecbaaab6c75bab
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-02-18 03:50:09 +0600

    (feat) added and updated hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,27 @@
-
+import { IoPaperPlaneOutline } from 'react-icons/io5'
+import HeroBackground from '/hero-background.png'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <>
+      <section id="hero">
+        <div className="max-w-[1610px] mx-auto">
+          <div className="wrapper relative">
+            <img src={HeroBackground} alt="" />
+            <div className="absolute top-24 left-[75px]">
+              <h1 className='text-[#253D4E] max-w-[680px] text-[72px] leading-[72px] font-bold mb-[31px]'>Don’t miss amazing grocery deals</h1>
+              <p className='font-lato text-[#7E7E7E] text-[30px] leading-[24px] mb-[65px]'>Sign up for the daily newsletter</p>
+              <div className="flex items-center h-[64px] w-[40vw] bg-white rounded-[50px] pl-7 gap-x-2">
+                <IoPaperPlaneOutline className='text-[#838383] text-2xl'/>
+                <input className='w-full h-full font-lato text-[#838383] outline-none' type="text" placeholder='Your email address' />
+                <button className='px-10 h-full bg-greeny text-white rounded-[50px] cursor-pointer'>Subscribe</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </>
   )
 }
 
